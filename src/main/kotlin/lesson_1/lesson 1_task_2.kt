9cf37bcc9066b94e88daa769b8a4c6a4a2d049b6
Edward <vikuloveduard5@gmail.com>
2025-06-08 14:43:34 +0300

    KS-1-2 added a variable with the number of visitors and output to the console

---
+++ src/main/kotlin/lesson_1/lesson 1_task_2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val numberOfOrders: Int = 75
+    val gratitudeForBuy: String = "Большое спасибо за покупку!!!"
+    var numberOfEmployees: Int = 2000
+
+    numberOfEmployees = 1999
+
+    println(numberOfOrders)
+    println(gratitudeForBuy)
+    println(numberOfEmployees)
+}
+
