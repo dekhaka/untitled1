5e44e14e2ed58818be5997f1bcea585b6b8286d5
Edward <vikuloveduard5@gmail.com>
2025-06-09 14:30:41 +0300

    KS-1-5 исправлены замечания

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -1,13 +1,15 @@
 fun main(){
     val fullSeconds: Int = 5409
-    val hours: Int = fullSeconds / 3600
-    val remainingSecondsAfterHours: Int = fullSeconds % 3600
-    val minutes: Int = remainingSecondsAfterHours / 60
-    val remainingSecondsAfterMinutes: Int = remainingSecondsAfterHours % 60
-    val seconds: Int = remainingSecondsAfterMinutes % 60
+    val hours: Int = fullSeconds / SECOND_IN_HOUR
+    val remainingSecondsAfterHours: Int = fullSeconds % SECOND_IN_HOUR
+    val minutes: Int = remainingSecondsAfterHours / SECOND_IN_MINUTE
+    val remainingSecondsAfterMinutes: Int = remainingSecondsAfterHours % SECOND_IN_MINUTE
+    val seconds: Int = remainingSecondsAfterMinutes % SECOND_IN_MINUTE
 
-    val formatTime: String = "$hours:$minutes:$seconds"
+    val formatTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
 
     println(formatTime)
 
 }
+const val SECOND_IN_MINUTE = 60
+const val SECOND_IN_HOUR = 60 * SECOND_IN_MINUTE
