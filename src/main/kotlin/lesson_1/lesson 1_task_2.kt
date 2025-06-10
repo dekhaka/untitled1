94226a7a5dbea09a54f65cf5bf648f12e19f8d13
Edward <vikuloveduard5@gmail.com>
2025-06-10 20:52:47 +0300

    KS-1-2 исправлены замечания от 10.06.2025

--- src/main/kotlin/lesson_1/lesson 1_task_2.kt
+++ src/main/kotlin/lesson_1/lesson 1_task_2.kt
@@ -2,6 +2,7 @@ fun main() {
     val numberOfOrders: Int = 75
     val gratitudeForBuy: String = "Большое спасибо за покупку!!!"
     var numberOfEmployees: Int = 2000
+    //println(numberOfEmployees)
 
     numberOfEmployees = 1999
 
